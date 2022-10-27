1a0993f40f3c0c9bc35f85af73d478fb786ea2c6
Runbo Jiang <runboj@andrew.cmu.edu>
2022-10-26 21:58:47 -0400

    Clear bugs in unet

--- models/unet.py
+++ models/unet.py
@@ -6,29 +6,27 @@ from distutils.command.build_clib import build_clib
 from unet_parts import *
 
 class Unet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+    def __init__(self, n_channels, n_classes):
         super(Unet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
-        self.biliear = bilinear
 
         self.inc = DoubleConv(n_channels, 64)
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
-        factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
+        self.down4 = Down(512, 1024)
 
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
+        self.up1 = Up(1024, 512)
+        self.up2 = Up(512, 256)
+        self.up3 = Up(256, 128)
+        self.up4 = Up(128, 64)
         self.outc = outConv(64, n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
-        x2 = self.down1(x2)
-        x3 = self.down2(x1)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
         x = self.up1(x5, x4)
@@ -46,7 +44,7 @@ class Unet(nn.Module):
 if __name__ == "__main__":
     # A full forward pass
     im = torch.randn(1, 1, 572, 572)
-    model = Unet()
+    model = Unet(1, 1)
 
     print(list(model.children()))
     import time
